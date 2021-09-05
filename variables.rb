b1dd7b31ff6f09435895a2df1ed5bf5b80735168
Ruben Pasillas <hobbbes@protonmail.com>
2021-09-04 20:46:11 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,33 @@
+# 2
+movie = "The Breakfast Club"
+
+# 2
+is_entertaining = true
+
+# Reassignment
+movie = "Silence of The Lambs"
+
+# 5 & 6
+num_4 = 4
+num_6 = 2
+
+
+# 7
+
+product = num_6 * num_4
+
+
+# 8
+
+favorite_foods = ["Quiche", "Oatmeal", "Pastrami"]
+puts favorite_foods
+
+# 9
+
+favorite_foods.shift()
+puts favorite_foods
+
+# 10
+
+total = movie.length()
+puts total
