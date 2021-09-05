65206d0a6822f5a8ca816ef674317d6150aa876f
Ruben Pasillas <hobbbes@protonmail.com>
2021-09-04 20:57:01 -0600

    Add developer hash

---
+++ models/model_b.rb
@@ -0,0 +1,16 @@
+DEVELOPER = {
+
+"name" => "Ruben P.",
+"nickname" => "Hobbes",
+
+"age" => 18,
+"years_of_experience" => 1,
+
+"is_fluent_in_ruby" => false,
+"understands_general_concepts" => true,
+
+"language_history" => ["C++", "C", "JS"],
+"lanaguges_to_learn" => ["Ruby", "C++", "Python"]
+
+
+}
