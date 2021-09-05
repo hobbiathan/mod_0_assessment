d7e7671df11036b60d8717d87c1ce69126599ced
Ruben Pasillas <hobbbes@protonmail.com>
2021-09-04 20:53:47 -0600

    Add dinosaur hash

---
+++ models/model_a.rb
@@ -0,0 +1,18 @@
+DINOSAUR = {
+
+"name" => "Pterodactyl",
+"size" => "Reasonably large"
+
+"estimated_population" => 30923,
+"known_population" => 1385,
+
+"is_extinct" => true,
+"is_biped" => false,
+
+"order_and_suborder" => ["Pterosauria", "Pterodactyloidea"],
+"known_species" => ["P. antiquus", "P. kochi", "P. scolopaciceps"]
+
+
+
+
+}
